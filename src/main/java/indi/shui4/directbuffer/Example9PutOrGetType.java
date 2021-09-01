334d7d986eaaa7b53d8f967bb3216cd325c97d97
DESKTOP-6OQ48GM\38482 <zw_xiao@zzyq.cc>
2021-09-01 13:40:58 +0800

    feature: putType （）和 getType （）万法的使用

---
+++ src/main/java/indi/shui4/directbuffer/Example9PutOrGetType.java
@@ -0,0 +1,27 @@
+package indi.shui4.directbuffer;
+
+import indi.shui4.util.BufferUtil;
+
+import java.nio.ByteBuffer;
+
+/**
+ * 1)getType: {@link ByteBuffer#putChar(char)} ByteBuffer#putXX(xx).. <br>
+ * 2)putType: {@link ByteBuffer#getChar(int)} ByteBuffer#getXX(xx)..
+ *
+ * @author shui4
+ * @since 2021/9/1(1.0)
+ */
+public class Example9PutOrGetType {
+
+  public static void main(String[] args) {
+    final ByteBuffer bb = ByteBuffer.allocate(10);
+    bb.putLong(Long.MAX_VALUE);
+    // position:8
+    System.out.println(bb);
+    System.out.println(bb.getLong(0));
+    bb.position(0);
+    System.out.println(bb.getLong());
+    // 0,0,0,0,0,0,0,1,0,0
+    System.out.println(BufferUtil.getContent(bb));
+  }
+}
