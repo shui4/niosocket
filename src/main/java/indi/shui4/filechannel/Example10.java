8ed1922d4259cbffe146c7c48e777d004ee3ffda
shui4 <1078185271@qq.com>
2021-09-06 15:57:56 +0800

    feature: 2.4.10 截断缓冲区

---
+++ src/main/java/indi/shui4/filechannel/Example10.java
@@ -0,0 +1,51 @@
+package indi.shui4.filechannel;
+
+import indi.shui4.util.FileUtil;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author shui4
+ * @since 2021/9/6(1.0)
+ */
+public class Example10 {
+
+  // 文件内容多余要截取的长度
+  @Test
+  public void case1() {
+    final ByteBuffer buffer1 = ByteBuffer.wrap("12345678".getBytes());
+    try (FileOutputStream fileOutputStream =
+            new FileOutputStream(FileUtil.getBuildPath("filechannel/newtext.txt"));
+        final FileChannel channel = fileOutputStream.getChannel()) {
+      channel.write(buffer1);
+      System.out.println("A size=" + channel.size() + " position=" + channel.position());
+      channel.truncate(3);
+      System.out.println("B size=" + channel.size() + " position=" + channel.position());
+      // 文件内容：123
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  // 文件内容小于要截取的长度
+  @Test
+  public void case2() {
+    final ByteBuffer buffer1 = ByteBuffer.wrap("12345678".getBytes());
+    try (FileOutputStream fileOutputStream =
+            new FileOutputStream(FileUtil.getBuildPath("filechannel/abc.txt"));
+        final FileChannel channel = fileOutputStream.getChannel()) {
+      channel.write(buffer1);
+      System.out.println("A size=" + channel.size() + " position=" + channel.position());
+      channel.truncate(30_000);
+      System.out.println("B size=" + channel.size() + " position=" + channel.position());
+      // 文件内容：12345678
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
