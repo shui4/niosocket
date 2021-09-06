1f6a76254355ac65aae8e76b58e7a7197482cebd
shui4 <1078185271@qq.com>
2021-09-07 00:37:41 +0800

    feature:
    2.4.19 将通道文件区域 接映射到内存

---
+++ src/main/java/indi/shui4/filechannel/Example20OpenFile.java
@@ -0,0 +1,19 @@
+package indi.shui4.filechannel;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.channels.FileChannel;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+
+/**
+ * {@link FileChannel#open(Path, OpenOption...)}
+ *
+ * @author shui4
+ * @since 1.0
+ */
+public class Example20OpenFile {
+
+  @Test
+  public void case1() {}
+}
