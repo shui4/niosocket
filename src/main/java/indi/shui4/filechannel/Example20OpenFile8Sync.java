3050cad90f34013bb96157343f3b8074842ffc58
shui4 <1078185271@qq.com>
2021-10-13 13:54:34 +0800

    feature: DSYNC Demo

--- src/main/java/indi/shui4/filechannel/Example20OpenFile8Sync.java
+++ src/main/java/indi/shui4/filechannel/Example20OpenFile8Sync.java
@@ -49,4 +49,22 @@ public class Example20OpenFile8Sync {
       e.printStackTrace();
     }
   }
+
+
+  /**
+   * 使用DSYNC的耗时：147.6 ms
+   */
+  @Test
+  public void case3DSync() {
+    final Path path = new File(FileUtil.getBuildPath("aaa.txt")).toPath();
+    try (FileChannel channel = FileChannel.open(path, DSYNC, CREATE_NEW, WRITE)) {
+      final Stopwatch stopwatch = Stopwatch.createStarted();
+      for (int i = 0; i < 200; i++) {
+        channel.write(ByteBuffer.wrap("a".getBytes()));
+      }
+      System.out.println(stopwatch);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
